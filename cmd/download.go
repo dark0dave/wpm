0f1b434b41c3b4348062f790ebfd94cf00adc6bb
dark0dave <dark0dave@mykolab.com>
2026-05-06 19:06:04 +0100

    feat(checksum): add checksum
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- cmd/download.go
+++ cmd/download.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"errors"
-	s "log/slog"
+	log "log/slog"
 
 	"github.com/dark0dave/wpm/pkg/git"
 	"github.com/dark0dave/wpm/pkg/manifest"
 	"github.com/dark0dave/wpm/pkg/url"
+	"github.com/dark0dave/wpm/pkg/util"
 	"github.com/spf13/cobra"
 	"golang.org/x/sync/errgroup"
 )
@@ -21,10 +22,10 @@ var (
 from wpm.yaml file to the weidu_modules folder`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			for _, dep := range m.Dependencies {
-				slog.Debug("Dependency", s.Any("dependency", dep))
+				log.Debug("Dependency", log.Any("dependency", dep))
 				wg.Go(func() error {
 					if err := download(&dep, FolderPath); err != nil {
-						slog.Error("Failed to download", s.Any("error", err))
+						log.Error("Failed to download", log.Any("error", err))
 						return err
 					}
 					return nil
@@ -39,10 +40,26 @@ func download(d *manifest.Dependency, folderPath string) error {
 	switch d.Protocol {
 	case manifest.Git:
 		d := git.Dependency{Dependency: d}
-		return d.Download(folderPath)
+		if err := d.Download(folderPath); err != nil {
+			return err
+		}
+		checksum, err := util.CheckSum(folderPath)
+		if err != nil {
+			return err
+		}
+		d.CheckSum = checksum
+		return nil
 	case manifest.Url:
 		d := url.Dependency{Dependency: d}
-		return d.Download(folderPath)
+		if err := d.Download(folderPath); err != nil {
+			return err
+		}
+		checksum, err := util.CheckSum(folderPath)
+		if err != nil {
+			return err
+		}
+		d.CheckSum = checksum
+		return nil
 	default:
 		return errors.New("unsupported protocol")
 	}
