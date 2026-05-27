4163d903772450da50ac8df2c97cfde36dc50d29
dark0dave <dark0dave@mykolab.com>
2026-05-28 03:22:57 +0900

    refactor(components): better comonents
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- pkg/manifest/component_test.go
+++ pkg/manifest/component_test.go
@@ -12,15 +12,34 @@ func TestParsing(t *testing.T) {
 		name, expected string
 	}{
 		{
+			name: "name, subComponent, componentName, tpfile test",
+			input: &WeiduComponent{
+				tpFile:        "test.tp2",
+				name:          "test",
+				subComponent:  "test",
+				componentName: "test",
+			},
+			expected: fmt.Sprintf("~test%ctest.tp2~ #0 #0 // test -> test", os.PathSeparator),
+		},
+		{
+			name: "name and tpfile test",
 			input: &WeiduComponent{
 				tpFile: "test.tp2",
 				name:   "test",
 			},
-			expected: fmt.Sprintf("~test%ctest.tp2~ #0 #0 // : ", os.PathSeparator),
+			expected: fmt.Sprintf("~test%ctest.tp2~ #0 #0", os.PathSeparator),
 		},
 		{
+			name: "tpfile only test",
+			input: &WeiduComponent{
+				tpFile: "test.tp2",
+			},
+			expected: "~test.tp2~ #0 #0",
+		},
+		{
+			name:     "empty test",
 			input:    &WeiduComponent{},
-			expected: fmt.Sprintf("~%c~ #0 #0 // : ", os.PathSeparator),
+			expected: "~~ #0 #0",
 		},
 	}
 	for _, tc := range testCases {
